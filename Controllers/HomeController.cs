213dab93c67cc0ccc6719056ca621df547f6870d
Thomas Volden <thvo@itu.dk>
2023-06-12 22:17:06 +0200

    Updated .net version to 7

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using FlatoutCMS_Sample.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FlatoutCMS_Sample.Controllers
 {
